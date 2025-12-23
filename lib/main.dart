bf07bc1f83f649b4b715211efc289f74c3055a31
Ahmad Said <ahmadbensaid205@gmail.com>
2025-12-23 12:53:16 +0200

    Initial Flutter widgets example

--- lib/main.dart
+++ lib/main.dart
@@ -28,70 +28,137 @@ class SimpleProject extends StatelessWidget {
         centerTitle: true,
         title: const Text(
           "ما شاء الله",
-          style: TextStyle(fontSize: 20, color: Colors.black),
+          style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 0, 0, 0)),
         ),
         leading: IconButton(
           onPressed: () {},
-          icon: const Icon(Icons.menu, size: 26, color: Colors.black),
+          icon: const Icon(
+            Icons.menu,
+            size: 26,
+            color: Color.fromARGB(255, 0, 0, 0),
+          ),
         ),
         actions: [
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.message, size: 23, color: Colors.black),
+            icon: const Icon(
+              Icons.message,
+              size: 23,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
           ),
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.search, size: 26, color: Colors.black),
+            icon: const Icon(
+              Icons.search,
+              size: 26,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
           ),
         ],
       ),
-
-      body: Container(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Container(
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(bottom: 20),
                 alignment: Alignment.center,
-                child: Text("ameer", style: TextStyle(fontSize: 50)),
-                color: Colors.amber[200],
-                height: 160,
-                width: 200,
-              ),
-            ),
-
-            Expanded(
-              flex: 1,
-              child: Container(
-                alignment: Alignment.center,
-                child: Text(
-                  "ameer",
-                  style: TextStyle(fontSize: 50, color: Colors.white),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blueGrey,
                 ),
-                color: Colors.blue[300],
-                height: 160,
-                width: 200,
-              ),
-            ),
-
-            Expanded(
-              flex: 2,
-              child: Container(
-                alignment: Alignment.center,
-                child: Text(
-                  "ameer",
-                  style: TextStyle(fontSize: 50, color: Colors.white),
+                height: 350,
+                width: double.infinity,
+                child: const Text(
+                  "اللهم صلي على سيدنا محمد",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
                 ),
-                color: Colors.red[300],
-                height: 160,
-                width: 200,
               ),
-            ),
-          ],
+
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blueGrey,
+                      ),
+                      height: 80,
+                      width: 140,
+                      child: const Text(
+                        "alaa",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 50),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blueGrey,
+                      ),
+                      height: 80,
+                      width: 140,
+                      child: const Text(
+                        "Said",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 50),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blueGrey,
+                      ),
+                      height: 80,
+                      width: 140,
+                      child: const Text(
+                        "saji",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 50),
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.blueGrey,
+                      ),
+                      height: 80,
+                      width: 140,
+                      child: const Text(
+                        "ameer",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 19),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blueGrey,
+                ),
+                height: 350,
+                width: double.infinity,
+                child: const Text(
+                  "الحمد لله",
+                  style: TextStyle(color: Colors.white, fontSize: 45),
+                ),
+              ),
+            ],
+          ),
         ),
-        color: Colors.blueGrey,
-        height: 500,
-        width: double.infinity,
       ),
     );
   }
